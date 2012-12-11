6a957e1813672c02158ec68c93570dc84aa8042f
KJlmfe <kjlmfe@gmail.com>
2012-12-11 16:58:53 +0800

    fix small bug of NO_OLD_ID

--- app/actions/SEStudent.php
+++ app/actions/SEStudent.php
@@ -11,6 +11,18 @@ class SEStudent{
 		SECommon::set_unread_msg_num();
 	}
 
+
+
+	function praise_event($eid)
+	{
+		$uid = Student::the_user_id():
+		PraiseList::add($uid, $eid);
+	}
+
+	function join_event($eid)
+	{
+
+	}
 }
 
 ?>
